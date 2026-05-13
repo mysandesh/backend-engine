b6788d9fc4ab776945dbff12f4528fc21468ee0a
Sandesh Acharya <mysandesh@gmail.com>
2026-05-13 10:49:11 +0545

    First API endpoint created

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
 const PORT = 5001;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
