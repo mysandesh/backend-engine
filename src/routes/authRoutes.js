8a6faba0c94ee7d4f0dce7baa661566e4f58c3fa
Sandesh Acharya <mysandesh@gmail.com>
2026-05-30 21:54:46 +0545

    Auth route message added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/");
+router.get("/", (req, res) => {
+  res.json({ httpMethod: "get", message: "This is the auth route" });
+});
 
 export default router;
