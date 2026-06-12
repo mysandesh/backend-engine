fec43994b758d1bdf10fe56c5e7df16a00385906
Sandesh Acharya <mysandesh@gmail.com>
2026-06-12 10:23:47 +0545

    Cookies set for user auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -35,7 +35,7 @@ const register = async (req, res) => {
   });
 
   //Generate JWT Token
-  const token = generateToken(user.id);
+  const token = generateToken(user.id, res);
 
   res.status(201).json({
     status: "success",
@@ -76,7 +76,7 @@ const login = async (req, res) => {
   }
 
   //Generate JWT Token
-  const token = generateToken(user.id);
+  const token = generateToken(user.id, res);
 
   res.status(201).json({
     status: "success",
